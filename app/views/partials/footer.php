8e95ba9738eb9c98c2772e132b4955a228296226
michalm57 <mroczekmichal57@gmail.com>
2023-06-10 17:31:41 +0200

    create footer

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -1,2 +1,8 @@
 </body>
+<footer id="footer" class="flex-shrink-0 py-4 bg-dark text-white-50">
+    <div class="container text-center">
+        <small>© <?php echo date('Y'); ?> Currency Exchange. All rights reserved.</small>
+    </div>
+</footer>
+
 </html>
