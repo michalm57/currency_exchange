e4273c0480e5940cf1964c1eacbe3971b30092bd
michalm57 <mroczekmichal57@gmail.com>
2023-06-17 19:29:14 +0200

    view for game

---
+++ app/views/game.view.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+
+<head>
+    <meta charset="utf-8" />
+
+    <!-- JS -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+    <script type="text/javascript" src="public/js/hangman.js"></script>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="public/css/hangman.css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inconsolata&display=swap"> 
+
+    <!-- TITLE -->
+    <title>Hangman</title>
+
+</head>
+
+<body>
+<div class="wrapper">
+    <div id="board"></div>
+    <div id="gallows">
+        <img src="public/images/image_0.png"  width="400"/>
+    </div>
+    <div id="alphabet"></div>
+    <div class="clear-both"></div>
+</div>
+</body>
+
+</html>
